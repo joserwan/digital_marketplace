fee1de651926e90222bb46f6fc1c5380f560ac85
Erwan Jossic <joserwan@gmail.com>
2021-07-05 16:27:10 -0400

    FIX: Lint

--- src/front-end/typescript/lib/app/view/nav.tsx
+++ src/front-end/typescript/lib/app/view/nav.tsx
@@ -243,7 +243,7 @@ const MobileAccountMenu: View<Props> = props => {
       {actions.map((action, i, arr) => {
         const clonedAction = { ...action };
         if (clonedAction.tag === 'link' && clonedAction.value.button) {
-          clonedAction.value = { ...clonedAction.value, size: 'sm' };
+          clonedAction.value = { ...clonedAction.value, size: 'sm' as const };
         }
         const active = isAccountActionActive(action);
         const button = isAccountActionButton(action);
